e256cc7b0a5848b55365700c97c4f27dd7813c24
Yohann Balawender <yohann.balawender@gmail.com>
2020-10-02 19:13:42 +0200

    Better JS file management
    Separate auth script from other JS code.
    Now, the auth script is only loaded when the user is not connected.

--- src/website/static/js/app.js
+++ src/website/static/js/app.js
@@ -89,36 +89,6 @@ $(document).ready(() => {
         })
     })
 
-    $('#submit-auth').click((e) => {
-        const login = $('#login').val()
-        const password = $('#pwd').val()
-
-        $('form').find('.form-group').removeClass('has-danger');
-
-        $.ajax({
-            url: '/auth',
-            type: 'POST',
-            contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
-            data: {
-                'login': login,
-                'password': password,
-            },
-            dataType: 'json',
-            xhrFields: {
-                withCredentials: true
-            },
-            crossDomain: true
-        })
-        .done(function(data) {
-            document.location.href = '/';
-        })
-        .fail(function(err) {
-            $('form').find('.form-group').addClass('has-danger');
-        })
-
-        e.preventDefault()
-    })
-
     if (window.BLOCKCHAIN_APP.email) {
         $.post('/users/list', { })
         .done((users) => {
